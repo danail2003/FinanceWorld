661833d2f819abb797bbe85e375d09007faf6811
danail2003 <ddobrev20@outlook.com>
2021-10-31 10:49:01 +0200

    course entity is initialized

---
+++ Data/FinanceWorld.Data.Models/Course.cs
@@ -0,0 +1,13 @@
+﻿namespace FinanceWorld.Data.Models
+{
+    using FinanceWorld.Data.Common.Models;
+
+    public class Course : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+    }
+}
