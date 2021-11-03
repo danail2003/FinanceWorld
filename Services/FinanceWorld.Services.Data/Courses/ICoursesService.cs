dde034b604b680efc918b63931946d39bc581b10
danail2003 <ddobrev20@outlook.com>
2021-11-03 19:59:57 +0200

    implement the courses service interface

---
+++ Services/FinanceWorld.Services.Data/Courses/ICoursesService.cs
@@ -0,0 +1,20 @@
+﻿namespace FinanceWorld.Services.Data.Courses
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using FinanceWorld.Data.Models;
+
+    public interface ICoursesService
+    {
+        Task<Course> CreatAsync();
+
+        IEnumerable<T> GetAll<T>();
+
+        T GetById<T>(int id);
+
+        Task<Course> UpdateAsync(int id);
+
+        Task<int> DeleteAsync(int id);
+    }
+}
