2df22a3aa4f89c669d13c28f20e64e7753e3b428
danail2003 <ddobrev20@outlook.com>
2021-11-24 20:31:06 +0200

    added implementation for all courses view

---
+++ Web/FinanceWorld.Web.ViewModels/Courses/CoursesViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FinanceWorld.Web.ViewModels.Courses
+{
+    public class CoursesViewModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+    }
+}
