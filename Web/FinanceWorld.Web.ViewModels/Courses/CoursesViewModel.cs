784de748619acde320341a61db243217d6b1426d
danail2003 <ddobrev20@outlook.com>
2021-11-28 18:20:27 +0200

    bug fixed and removed redundant model

--- Web/FinanceWorld.Web.ViewModels/Courses/CoursesViewModel.cs
+++ Web/FinanceWorld.Web.ViewModels/Courses/CoursesViewModel.cs
@@ -1,5 +1,7 @@
 ﻿namespace FinanceWorld.Web.ViewModels.Courses
 {
+    using System.ComponentModel.DataAnnotations;
+
     using FinanceWorld.Data.Models;
     using FinanceWorld.Services.Mapping;
 
@@ -7,10 +9,16 @@
     {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(50)]
+        [MinLength(5)]
         public string Name { get; set; }
 
+        [Required]
+        [MinLength(10)]
         public string Description { get; set; }
 
+        [Range(1, 5000)]
         public double Price { get; set; }
     }
 }
